081f292b9893be26fd5d08e79b61b659172b8239
Balaa2801 <balajibabu8122@gmail.com>
2024-05-21 18:00:03 +0530

    updated deploy.sh and Jenkinsfile for 20th build

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# Pull the dev Docker image from Docker Hub
+# Navigate to the directory containing docker-compose.yml
+cd "$(dirname "$0")"
+
+# Pull the dev Docker image from Docker Hub and start the services
 docker-compose up -d
 
